eceb4a01809d1533b6ac7447948bf2cdabbaee2d
Vlad <snozyk.v@gmail.com>
2020-02-28 17:44:37 +0100

    ps done

--- main.py
+++ main.py
@@ -6,8 +6,18 @@ import matplotlib.pyplot as plt
 import itertools as it
 import scipy
 
-sample1 = np.random.normal(0,1,5)
-plt.plot(sample1)
-plt.show()
 
-a=2+2
+def prob(x, N = 10000):
+	sample = []
+	for i in range(N):
+		sum = 0
+		while sum <= x:
+			sum += np.random.uniform(0, 1, 1)
+		if sum > 1:
+			sample.append(0)
+		else:
+			sample.append(1)
+	return np.mean(sample)
+
+xs = np.linspace(0,1,num = 100000)
+ps = np.array([prob(x) for x in xs])
